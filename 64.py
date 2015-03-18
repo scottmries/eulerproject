a5f60fbe2d77b87c5920206b0fb95e647b1cd7df
Wendy Carlos Williams <scottmries@gmail.com>
2015-03-18 09:55:14 -0400

    Update 64.py

--- 64.py
+++ 64.py
@@ -1,43 +1,75 @@
 import math
 
-# sqrt(x)
-# floor(sqrt(x)) + sqrt(x) - floor(sqrt(x))
-# floor(sqrt(x)) + 1/(1/(sqrt(x)-floor(sqrt(x)))) = floor(sqrt(x)) + 1/(sqrt(x)-(sqrt(x)+floor(sqrt(x))/(sqrt(x)**2-(floor(sqrt(x)))**2))
+def e_sieve(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
 
-# for 1/(sqrt(23)-4), input is (23,4,1), output is (1,23,3,7)
-def root_denom(x,y,z):
-	# 1/(x**(1/2)-y) => (x**(1/2)+y)/(x-y**2) => floor((x**(1/2)+y)/(x-y**2)) + (x**(1/2)-y**2*(floor((x**(1/2)+y)/(x-y**2))))
-	print x,y,x-y**2
-	a = (x**(1/2)+y)/(x-y**2)
-	b = x
-	c = y**2*(x**(1/2)+y)
-	d = x-y**2
-	return (a,b,c,d)
+primes = e_sieve(10000)
+
+def gcf(n,d):
+	gcf = 1
+	test_primes = [p for p in primes if p<=min(n,d)]
+	for p in test_primes:
+		while True:
+			if n%p==0 and d%p==0:
+				n /= p
+				d /= p
+				gcf*=p
+			else:
+				break
+	return gcf
+
+def next_simple_cf_term((x,y,z)):
+	# input is (sqrt(x)-y)/z
+	# take the highest integer below that quotient as an
+	an = int(math.floor((math.sqrt(x)+y)/z))
+
+
+	# sqrt(x) = an + (math.sqrt(x)+y-an*z)/z
+	y -= an*z
+	# sqrt(x) = an + (math.sqrt(x) +y)/z
+
+	# z/(math.sqrt(x) + y)
+	# z*(math.sqrt(x) - y)/(x-y**2)
+
+	f = gcf(z,(x-y**2))
+	z = (x-y**2)/f
+
+	return (an,(x,abs(y),z))
+
+
+
+def sqrt_expansion(n):
+	args = (n,0,1)
+	full_expansion = list()
+	while True:
+		next_expansion = next_simple_cf_term(args)
+		full_expansion.append(next_expansion[0])
+
+		if next_expansion[0] == 2*full_expansion[0]:
+			break
+		else:
+			args = next_expansion[1]
+	return full_expansion
 
 # for all quadratic irrationals, the first non-repeating number is floor(sqrt(x))
 # 	and the last of the period is 2*floor(sqrt(x)).
 # 	a quality of the digits between these is that they are symmetrical,
-# 	but the appear of 2*floor(sqrt(x)) may be sufficient?
+# 	but the appearance of 2*floor(sqrt(x)) may be sufficient?
 
-print (9**(1/2))
-print (10**(1/2))
+def is_square(i):
+	return math.floor(i**0.5)**2==i
 
 solution = 0
 
-# for i in range(1,10000):
-for i in range(1,10):
-	expansion = [i**(1/2)]
-	args = [i,i**(1/2),1]
-	while True:
-		next_result = root_denom(args[0],args[1],args[2])
-		expansion.append(next_result[0])
-		print expansion
-		if next_result[0] == 2*i**(1/2):
-			length = len(expansion)-1
-			if length%2==0:
-				solution += 1
-			print "expansion of %s is %s" % (i,"["+expansion[0]+","+expansion[1:]+"]")
-			break
-		else:
-			args = [next_result[1],next_result[2],next_result[3]]
+for i in range(1,10001):
+	if not is_square(i):
+		print sqrt_expansion(i)
+		if (len(sqrt_expansion(i))-1)%2==1:
+			solution += 1
 
+print solution
