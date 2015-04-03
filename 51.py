40dc34f5e0f81dc2f6ac26333c5c6efc9423b566
Wendy Carlos Williams <scottmries@gmail.com>
2015-04-03 08:23:17 -0400

    I'm never gonna solve this.

--- 51.py
+++ 51.py
@@ -1,13 +1,55 @@
-# this one's a doozy, and it's still not done....
+import itertools
 
-# we know that the last digit cannot be one of the ones that changes (only 1,3,7, and 9 can end a prime; there aren't 8 possibilities)
+nc = [[h for h in str(i)] for i in range(100,100000) if i%3!=0]
+# print nc
+ncp = list(itertools.combinations(range(0,5),3))
+print ncp
 
-# s = sum of the non-changing digits
+def e_sieve(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
 
-# t = the replacing digit
+primes = [p for p in e_sieve(10000000) if p >=100000]
 
-# s%3   t    	2*t 		3*t
+def test(n):
+	l = list()
+	for i in range(10):
+		t = ''
+		for s in str(n):
+			if s=='x':
+				t += str(i)
+			else:
+				t += s
+		t = int(t)
+		if t in primes:
+			l.append(t)
+	return [k for k in l if k in primes]
 
-# 0		t%3!=0	(2*t)%3!=0  impossible!
-# 1		t%3!=2	(2*t)%3!=1
-# 2		t%3!=1	(2*t)%3!=2
+for m in ncp:
+	for n in nc:
+		s = ""
+		t_sum = 0
+		while True:
+			if len(s)<=len(n):
+				for e,o in enumerate(m):
+					# print o
+					if len(s) == o:
+						# print s,m,o
+						t_sum += int(n[e])
+						s+=n[e]
+					else:
+						s+='x'
+			else:
+				break
+		print s
+		jl = [s+str(k) for k in [1,3,7,9] if (k + t_sum)%3!=0]
+		for s in jl:
+			result = test(s)
+			if len(result)>3:
+				# print len(result),result
+				if len(result)>7:
+					quit()
