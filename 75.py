8ef23c988bbd32483db7d32099f1bcef46c197b8
Wendy Carlos Williams <scottmries@gmail.com>
2015-05-18 11:53:26 -0400

    solution using coprime pairs and integer factor k

--- 75.py
+++ 75.py
@@ -1,119 +1,59 @@
-# generate the Pythagorean triples less than 1500000
+limit = 1500000
 
-# for coprime m,n 
-# a = m**2 - n**2, b = 2mn, c = m**2 + n**2
-# if m-n is even, divide a,b, and c by two
+solution = 0
 
-# multiply a,b, and c by integers through to the limit
-# increment the value at a+b+c in a dictionary
+wirelengths = list()
+for i in xrange(0, limit+1):
+    wirelengths.append([])
 
-# generate the next coprime pair until a+b+c > 1500000
-
-# upper_bound = 1500000
-
-class Coprimes(object):
-
-	def __init__(self,limit):
-		self.roots = [(2,1),(3,1)]
-		self.primitive_triples = []
-		self.all_triples = []
-		self.generators = [self.generate_branch1,self.generate_branch2,self.generate_branch3]
-		self.int_triangles = dict()
-		self.limit = limit
-		
-
-	def primitive_pythag_triple(self,m,n):
-		a = m**2 - n**2
-		b = 2*m*n
-		c = m**2 + n**2
-		if (m-n)%2==0:
-			return a/2,b/2,c/2
-		else:
-			return a,b,c
-
-	def generate_branch1(self,m,n):
-		# print "b1",
-		n2 = m
-		m2 = 2*m-n
-		
-		# print self.primitive_pythag_triple(m2,n2)
-		return (m2,n2),self.primitive_pythag_triple(m2,n2)
-
-	def generate_branch2(self,m,n):
-		# print "b2",
-		n2 = m
-		m2 = 2*m+n
-		# print self.primitive_pythag_triple(m2,n2)
-		return (m2,n2),self.primitive_pythag_triple(m2,n2)
-
-	def generate_branch3(self,m,n):
-		# print "b3",
-		m2 = m+2*n
-		# print self.primitive_pythag_triple(m2,n)
-		return (m2,n),self.primitive_pythag_triple(m2,n)
-
-	def generate_with_prim_pythag_triple_sum_below(self):
-		
-		coprimes = list()
-		for r in self.roots:
-			for generator in self.generators:
-				m,n = r[0],r[1]
-				a,b,c = self.primitive_pythag_triple(m,n)
-				print generator
-				while True:
-					if a + b + c < self.limit:
-						self.primitive_triples.append((a,b,c))
-						# print generator(m,n)
-						g = generator(m,n)
-						(m,n),(a,b,c) = g[0],g[1]
-					else:
-						break
-
-	def triples_from_primitives_below(self):
-		
-		self.generate_with_prim_pythag_triple_sum_below()
-		for primitive in self.primitive_triples:
-			a,b,c = 0,0,0
-			# a,b,c = primitive[0], primitive[1], primitive[2]
-			while True:
-				a += primitive[0]
-				b += primitive[1]
-				c += primitive[2]
-				s = sum([a,b,c])
-				
-				if s < self.limit:
-					print a,b,c
-					if s in self.int_triangles:
-						if list(sorted([a,b,c])) not in self.int_triangles[s]:
-							self.int_triangles[s].append(list(sorted([a,b,c])))
-					else:
-						self.int_triangles[s] = [list(sorted([a,b,c]))]
-				
-					
-					# i += 1
-				else:
-					break
+coprime_pairs = list()
 
 
-	def count_single_solution_triangles(self):
-		
-		
-		self.triples_from_primitives_below()
-		# for t in self.int_triangles:
-			# if self.int_triangles[t] > 1:
-				# print "%s: %s" % (t, self.int_triangles[t])
-		
-		# print len(self.int_triangles.keys())
-		solution = 0
-		print len(self.int_triangles)
-		for k in self.int_triangles:
-			if len(self.int_triangles[k]) == 1:
-				solution += 1
-		return solution
+def farey(n, asc=True):
+    """Print the nth Farey sequence, either ascending or descending."""
+    if asc:
+        a, b, c, d = 0, 1,  1, n     # (*)
+    else:
+        a, b, c, d = 1, 1, n-1, n     # (*)
+    print "%d/%d" % (a, b)
+    while (asc and c <= n) or (not asc and a > 0):
+        k = int((n + b)/d)
+        a, b, c, d = c, d, k*c - a, k*d - b
+        # print a, b
+        if a < b:
+            coprime_pairs.append((a, b))
+            print "%d/%d" % (a, b)
 
-low_coprimes = Coprimes(100)
-# low_coprimes.generate_with_prim_pythag_triple_sum_below()
-# print low_coprimes.count_single_solution_triangles()
-coprimes = Coprimes(1500001)
-print coprimes.count_single_solution_triangles()
-print "120", coprimes.int_triangles[120]
+# m**2 + n**2 + m**2 - n **2 + 2*m*n <= limit
+# 2*m**2 + 2*m*n <= limit
+# m**2 + m*n <= limit/2
+# m + n <= limit/(2*m)
+
+# given this, the square root of the limit is sufficiently large
+
+farey(int(limit**0.5))
+
+for coprime_pair in coprime_pairs:
+    m = coprime_pair[1]
+    n = coprime_pair[0]
+    k = 1
+    while True:
+        a = k*(m**2 + n ** 2)
+        b = k*(2*m*n)
+        c = k*(m**2 - n ** 2)
+        wirelength = a + b + c
+        if wirelength <= limit:
+            triple = [a, b, c]
+            triple.sort()
+            if triple not in wirelengths[wirelength]:
+                wirelengths[wirelength].append(triple)
+            k += 1
+        else:
+            break
+
+for wirelength in wirelengths:
+    if len(wirelength) == 1:
+        solution += 1
+    print e, wirelength
+
+print solution
