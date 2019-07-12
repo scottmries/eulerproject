c104ba89009f48fdcd2d0d55484cc208ae333fec
scottmries <scottmries@gmail.com>
2019-07-11 22:42:52 -0400

    86 passes tests but needs to be optimized.

--- 86.py
+++ 86.py
@@ -1,15 +1,42 @@
-# for cube with sides m, n, and p,
-# the shortest path is the minimum of
-# ((m + n)^2 + p^2 )^(1/2)
-# ((m + p)^2 + n^2 )^(1/2)
-# ((n + p)^2 + m^2 )^(1/2)
+import math
 
-# given the winning length, 
-# if its corresponding sides are two of a Pythagorean triple,
-# (how to determine this?)
-# it is a solution
+M = 1
+solutions = 0
 
-# a recursive structure could be used to generate the cubes 
-# smaller than MxMxM
-# but not smaller than M-1xM-1xM-1
-# the solutions in this logical supplement can then simply be added to those found previously, etc.
+squares = [True, True, False]
+
+testDist = {1: {2: {}}}
+
+try:
+    print testDist[1][2][3]
+except:
+    print "nope"
+
+def isSquare(s):
+    return int(math.floor(math.sqrt(s)))**2 == s
+
+while True:
+    m = M
+    for n in range(1, m + 1):
+        for p in range(1, n + 1):
+            least = (m + n)**2 + p**2
+            if (m + p)**2 + n**2 < least:
+                least = (m + p)**2 + n**2
+            if (n + p)**2 + m**2 < least:
+                least = (n + p)**2 + m**2
+            if least > len(squares):
+                for s in range(len(squares), least + 1):
+                    squares.append(isSquare(s))
+            if squares[least]:
+                solutions += 1
+    print "%s: %s" %(M, solutions)
+    if M == 99 and solutions != 1975:
+        print "solutions(99) != 1975"
+        break
+    if M == 100 and solutions != 2060:
+        print "solutions(100) != 2060"
+        break
+    if solutions > 1000000:
+        print "solution: %s" %(M)
+        break
+    M += 1
