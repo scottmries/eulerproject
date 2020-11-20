c3253f89120fdb46a598ebfceae21dfbf8931045
Scott Ries <scottmries@gmail.com>
2020-11-19 23:55:12 -0500

    88 pseudo-code solution.

--- 88.py
+++ 88.py
@@ -1,94 +1,37 @@
-# -*- coding: UTF-8 -*-
+import numpy, sys, math
 
-# A natural number, N, that can be written as the sum and product of a given set of at least two natural numbers, {a1, a2, ... , ak} is called a product-sum number: N = a1 + a2 + ... + ak = a1 × a2 × ... × ak.
+k = sys.argv[0] if len(sys.argv) > 0 else 12000
 
-# For example, 6 = 1 + 2 + 3 = 1 × 2 × 3.
+# for some set S in the naturals, s > 1,
+# there is a unique productSum,
+# viz. S U {1} * (prod(S) - sum(S))
+# e.g.
+# S = {4, 3}
+# prod(S) = 12
+# sum(S) = 7
+# prodSum(S) = { 4, 3, 1, 1 ,1, 1, 1}
 
-# For a given set of size, k, we shall call the smallest N with this property a minimal product-sum number. The minimal product-sum numbers for sets of size, k = 2, 3, 4, 5, and 6 are as follows.
+# proof: if any s changes, the sum may be preserved, but S is not
+# if the number of ones changes, the product will be preserved, but the sum will not
 
-# k=2: 4 = 2 × 2 = 2 + 2
-# k=3: 6 = 1 × 2 × 3 = 1 + 2 + 3
-# k=4: 8 = 1 × 1 × 2 × 4 = 1 + 1 + 2 + 4
-# k=5: 8 = 1 × 1 × 2 × 2 × 2 = 1 + 1 + 2 + 2 + 2
-# k=6: 12 = 1 × 1 × 1 × 1 × 2 × 6 = 1 + 1 + 1 + 1 + 2 + 6
+# because the corresponding productSums of the combinations of factors of n are unique
+# incrementing k, and filling the least productSum has yet to be filled,
+# will solve this problem
 
-# Hence for 2≤k≤6, the sum of all the minimal product-sum numbers is 4+6+8+12 = 30; note that 8 is only counted once in the sum.
+# for some p, a potential least productSum, incrementing from 4
+# what are its prime factors?
+# what are the combinations of its prime factors?
+# for each combination of prime factors,
+# find the corresponding productSum
+# if the productSum for some digit-length k for which p has a productSum is not yet known
+# p is the least productSum for that k
 
-# In fact, as the complete set of minimal product-sum numbers for 2≤k≤12 is {4, 6, 8, 12, 15, 16}, the sum is 61.
+# repeat for the range
 
-# What is the sum of all the minimal product-sum numbers for 2≤k≤12000?
-
-# One strategy would be to iterate over 2->12000, finding every lowest PS number,
-# but there may be a way to anticipate redundant PS numbers ahead of time?
-
-import numpy
-
-maximumK = 100
-
-smallestProductSumsOfSize = {2: 2, 3: 6, 4: 8, 5: 8, 6: 12}
-
-possibleProductSumSets = {1: []}
-for i in range(1, 10):
-    possibleProductSumSets[1].append({
-        'set': [i],
-        'product': i,
-        'sum': i
-    })
-
-def getSmallestProductSumOfSize(k):
-    minimumPSNumber = None
-    set = None
-    for s in possibleProductSumSets[k]:
-        if s['product'] == s['sum']:
-            if s['product'] < minimumPSNumber or minimumPSNumber is None:
-                minimumPSNumber = s['product']
-                set = s['set']
-    return {'value': minimumPSNumber, 'set': set}
-
-def getNextSetOfDigits(sets):
-    nextSet = []
-    for s in sets:
-        lastValue = s['set'][-1]
-        for i in range(lastValue, 10):
-            # since these sets will be in ascending order,
-            copy = s['set'][:]
-            copy.append(i)
-            product = i * s['product']
-            sum = i + s['sum']
-            # if the set's product exceeds the set's sum,
-            # it will always exceed the set's sum;
-            # it's therefore not a possibility
-            # this condition is key:
-
-            # assume the rest of the digits are additively minimized by choosing i,
-            # does the product exceed the sum?
-            if product <= sum and product * i <= maximumK * 9:
-                # print(copy)
-                # print len(copy)
-                # print 'product', product
-                # print 'sum', sum
-                nextSet.append({
-                    'set': copy,
-                    'product': product,
-                    'sum': sum
-                })
-    return nextSet
-
-last = 0
-sum = 0
-values = []
-for i in range(2, maximumK):
-    possibleProductSumSets[i] = getNextSetOfDigits(possibleProductSumSets[i - 1])
-    print possibleProductSumSets[i]
-    smallest = getSmallestProductSumOfSize(i)
-    print i, smallest['value']
-    set = None
-    if smallest['value'] > last:
-        sum += smallest['value']
-        last = smallest['value']
-        set = smallest['set']
-    if last not in values:
-        values.append(last)
-    # print i, set
-print numpy.sum(values)
-print values
+def e_sieve(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3, int(n**0.5)+1, 2):
+        if sieve[i]:
+            sieve[i*i::2*i] = [False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3, n, 2) if sieve[i]]
